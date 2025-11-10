c15b8176d49267467573d7782c6ee918f6ee100b
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-11-10 09:43:47 -0300

    feat: estrutura inicial

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return <div>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/login'>Login / Criar conta</Link>
+    </nav>
+  </div>;
+};
+
+export default Header;
