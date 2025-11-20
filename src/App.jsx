6d55ca0fa01b81ade01f08289a5cc0961761f715
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-11-20 08:32:50 -0300

    refactor: header e footer movidos para o dir _partials

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+import Header from "./Components/_partials/Header";
+import Footer from "./Components/_partials/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 
