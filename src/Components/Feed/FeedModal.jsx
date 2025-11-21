6720783b22f94802e34c9781b24008b13725b540
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-11-21 16:18:29 -0300

    feat: feed

---
+++ src/Components/Feed/FeedModal.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./FeedModal.module.css";
+
+const FeedModal = () => {
+  return <div>FeedModal</div>;
+};
+
+export default FeedModal;
---
+++ src/Components/Feed/FeedPhotos.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./FeedPhotos.module.css";
+import FeedPhotoItem from "./FeedPhotoItem";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+
+const FeedPhotos = () => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({
+        page: 1,
+        total: 6,
+        user: 0,
+      });
+      const { response, json } = await request(url, options);
+      console.log(response);
+    }
+    fetchPhotos();
+  }, []);
+
+  if (error) {
+    return <Error error={error} />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+  if (data) {
+    return (
+        <ul className={`${styles.feed} animLeft`}>
+          {data.map(photo => {
+            return <FeedPhotoItem key={photo.id} photo={photo}/>
+          })}
+        </ul>
+    );
+  }
+  return null
+};
+
+export default FeedPhotos;
