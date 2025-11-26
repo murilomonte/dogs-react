b45157c1d004334a4a3c6a06467b42675ab7da08
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-11-26 20:29:36 -0300

    feat: perfil usuario

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { UserStorage } from "./UserContext";
 import User from "./Components/User/User";
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
