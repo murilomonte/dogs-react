50f8033f5a9112dab44cf6df5205fc255eb54629
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-11-26 20:35:17 -0300

    feat: error 404

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import User from "./Components/User/User";
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Photo from "./Components/Photo/Photo";
 import UserProfile from "./Components/User/UserProfile";
+import NotFound from "./Components/NotFound";
 
 const App = () => {
   return (
@@ -32,6 +33,7 @@ const App = () => {
             />
             <Route path="foto/:id" element={<Photo />} />
             <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
